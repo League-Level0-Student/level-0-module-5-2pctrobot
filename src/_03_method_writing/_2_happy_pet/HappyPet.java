b85f65631ac75ab00b34165fe60cd52ae6afe240
league <league@imac-18.attlocal.net>
2023-04-23 11:22:18 -0700

    Pat the cat

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,23 +5,25 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String petType = JOptionPane.showInputDialog("What pet do you want, Zeus the cat, Dash the Cat, fish, dog, snake, cat, or Nyx the Cat.");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
-		
+while(happinessLevel < 100) {
+	
+
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, petType, petType, 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Pat", "Walk", "Feed", "Water" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+}
 	}
 
 	// 5. Create methods to handle each of your user selections.
