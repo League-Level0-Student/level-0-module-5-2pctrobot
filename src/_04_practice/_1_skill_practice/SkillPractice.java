c464b8da5ec5a9f6addc4ba86d9220d6e557704c
league <alishahussain1229@gmail.com>
2023-05-14 11:19:10 -0700

    Skillz!

--- src/_04_practice/_1_skill_practice/SkillPractice.java
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -1,36 +1,42 @@
 package _04_practice._1_skill_practice;
 
+import javax.swing.JOptionPane;
+
 public class SkillPractice {
 public static void main(String[] args) {
-    skill1();
-    skill2();
-    skill3();
-    skill4();
-    skill5();
+skill1();
+skill2();
+skill3();
+skill4();
+skill5();
 }
 
 private static void skill5() {
-	// TODO Auto-generated method stub
 	
 }
 
 private static void skill4() {
-	// TODO Auto-generated method stub
 	
 }
 
 private static void skill3() {
-	// TODO Auto-generated method stub
 	
 }
 
 private static void skill2() {
-	// TODO Auto-generated method stub
 	
 }
 
 private static void skill1() {
-	// TODO Auto-generated method stub
-	
+	String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+	int dime = Integer.parseInt(dimes);
+	JOptionPane.showMessageDialog(null, "You have " + dime * 10 + " cents.");
+	String inches = JOptionPane.showInputDialog("How tall are you?");
+	int inch = Integer.parseInt(inches);
+	if(inch > 36) {
+		JOptionPane.showMessageDialog(null, "You are tall enough.");
+	}else {
+		JOptionPane.showMessageDialog(null, "DRINK MILK.");
+	}
 }
 }
